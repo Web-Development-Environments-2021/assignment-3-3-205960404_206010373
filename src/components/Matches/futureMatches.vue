b230fed5a5f9285d3e9b8eacb6200ca184e4f3a6
dvirainisch <63093025+dvirainisch@users.noreply.github.com>
2021-06-17 21:03:05 +0300

    currentStage

--- src/components/Matches/futureMatches.vue
+++ src/components/Matches/futureMatches.vue
@@ -1,16 +1,23 @@
 <template>
  <div>
    <h1>Future Games</h1>
-    <b-table :items="items" :fields="fields" striped responsive="sm">
+    <!-- <b-table :items="items" :fields="fields" striped responsive="sm"> -->
+        <b-table striped hover :items="items" :fields="fields">
+          <template v-slot:cell(home_team)="data">
+      <router-link :to="`/teams/teamFullDetails/{data.value}`">{{ data.value }}</router-link>
+    </template>
+    <template v-slot:cell(away_team)="data">
+      <router-link :to="`/teams/teamFullDetails/{data.value}`">{{ data.value }}</router-link>
+    </template>
       <template #cell(show_details)="row">
         <b-button size="sm" @click="row.toggleDetails" class="mr-2">
           {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
         </b-button>
-
+        <b-button @click="addMatchFav(row.item.MatchID)">Add Match to Favorites</b-button>
         <!-- As `row.showDetails` is one-way, we call the toggleDetails function on @change -->
-        <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
+        <!-- <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
           Details via check
-        </b-form-checkbox>
+        </b-form-checkbox> -->
       </template>
 
       <template #row-details="row">
@@ -19,7 +26,6 @@
             <b-col sm="3" class="text-sm-right"><b>Date:</b></b-col>
             <b-col>{{ row.item.Date }}</b-col>
           </b-row>
-
           <b-row class="mb-2">
             <b-col sm="3" class="text-sm-right"><b>Hour:</b></b-col>
             <b-col>{{ row.item.Hour }}</b-col>
@@ -28,7 +34,6 @@
             <b-col sm="3" class="text-sm-right"><b>Stadium:</b></b-col>
             <b-col>{{ row.item.Stadium }}</b-col>
           </b-row>
-
           <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
         </b-card>
       </template>
@@ -69,6 +74,11 @@ export default {
         console.log("error in update games")
         console.log(error);
       }
+    },
+    async addMatchFav(matchID){
+      console.log("*******addMatchFav*******");
+      console.log(matchID);
+      console.log(this.$root.store.username);
     }
 
   },
--- src/components/Matches/pastMatches.vue
+++ src/components/Matches/pastMatches.vue
@@ -1,16 +1,23 @@
 <template>
  <div>
    <h1>Past Games</h1>
-    <b-table :items="items" :fields="fields" striped responsive="sm">
+    <!-- <b-table  :items="items" :fields="fields" striped responsive="sm"> -->
+      <b-table striped hover :items="items" :fields="fields">
+          <template v-slot:cell(home_team)="data">
+      <router-link :to="`/teams/teamFullDetails/{data.value}`">{{ data.value }}</router-link>
+    </template>
+    <template v-slot:cell(away_team)="data">
+      <router-link :to="`/teams/teamFullDetails/{data.value}`">{{ data.value }}</router-link>
+    </template>
       <template #cell(show_details)="row">
         <b-button size="sm" @click="row.toggleDetails" class="mr-2">
           {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
         </b-button>
 
         <!-- As `row.showDetails` is one-way, we call the toggleDetails function on @change -->
-        <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
+        <!-- <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
           Details via check
-        </b-form-checkbox>
+        </b-form-checkbox> -->
       </template>
 
       <template #row-details="row">
@@ -19,7 +26,6 @@
             <b-col sm="3" class="text-sm-right"><b>Date:</b></b-col>
             <b-col>{{ row.item.Date }}</b-col>
           </b-row>
-
           <b-row class="mb-2">
             <b-col sm="3" class="text-sm-right"><b>Hour:</b></b-col>
             <b-col>{{ row.item.Hour }}</b-col>
@@ -27,6 +33,18 @@
           <b-row class="mb-2">
             <b-col sm="3" class="text-sm-right"><b>Stadium:</b></b-col>
             <b-col>{{ row.item.Stadium }}</b-col>
+          </b-row>
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>Result:</b></b-col>
+            <b-col>{{ row.item.HomeGoals }} - {{ row.item.AwayGoals }}</b-col>
+          </b-row>
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>Event Calendar:</b></b-col>
+            <b-col >
+              <ul>
+                <li v-for="g in row.item.eventCalendar" :key="g">{{ g.Description }} </li>
+                </ul>
+             </b-col>
           </b-row>
 
           <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
@@ -76,8 +94,5 @@ export default {
 </script>
 
 <style>
-
-
-
 
 </style>
