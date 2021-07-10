9ba30bc5d3ad237b833a14bff8c657a6baa5fd0f
liadse <58271429+liadse@users.noreply.github.com>
2021-07-10 11:56:08 +0300

    admin page start

--- src/pages/AdminsPage.vue
+++ src/pages/AdminsPage.vue
@@ -13,56 +13,78 @@
       <b-collapse id="collapse-1" class="mt-2">
         <div>
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          placeholder="Enter email"
-          required
-        ></b-form-input>
-      </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+       
+ 
+        <b-form-group id="input-group-2" label="Stadium Name:" label-for="input-2">
         <b-form-input
           id="input-2"
-          v-model="form.name"
+          v-model="form.stadium"
           placeholder="Enter name"
           required
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+      <b-form-group id="input-group-2" label="SuperLigaName:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.superligaName"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Season:" label-for="input-3">
         <b-form-select
           id="input-3"
-          v-model="form.food"
-          :options="foods"
+          v-model="form.seasonName"
+          :options="seasons"
           required
         ></b-form-select>
       </b-form-group>
 
-      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          v-model="form.checked"
-          id="checkboxes-4"
-          :aria-describedby="ariaDescribedby"
-        >
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
+        <b-form-group id="input-group-3" label="Stage:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.stageName"
+          :options="stages"
+          required
+        ></b-form-select>
       </b-form-group>
 
+      <b-form-group id="input-group-3" label="Referee Name:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.refereeName"
+          :options="referees"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Home Team Id:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.homaTeamID"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Away Team Id:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.awayteamID"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+
+
       <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+      <!-- <b-button type="reset" variant="danger">Reset</b-button> -->
     </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
+
   </div>
       </b-collapse>
       <br><br>
@@ -133,8 +155,12 @@
           food: null,
           checked: []
         },
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-        show: true
+        seasons: [{ text: 'Select One', value: null }, '2017/2018', '2018/2019', '2019/2020', '2020/2021'],
+        show: true,
+        stages: [{ text: 'Select One', value: null }, 'Regular Season', 'Play Off'],
+        referees: [{ text: 'Select One', value: null }, 'Pierluigi Collina', 'Markus Merk', 'Howard Webb','Kim Milton Nielsem','Sandor Puhl','Peter Mikkelsen','Michel Vautrot','Pedro Proenca','Oscar Ruiz','Frank de Bleeckere']
+        
+        
       }
     },
     methods: {
