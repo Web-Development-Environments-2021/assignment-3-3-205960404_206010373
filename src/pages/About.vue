140adc6e663fca34480d27d28b65254544397259
dvirainisch <63093025+dvirainisch@users.noreply.github.com>
2021-07-10 11:50:10 +0300

    photo

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,6 +7,7 @@
       As part of the Project we built the Backend side as well as the Frontend side.<br>
       for this project we learned JavaScript and Vue.<br>
     </h4>
+    <img src=>
   </div>
 </template>
 
